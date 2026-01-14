4cc187c319af5b61365a36a54e45c534b774480e
kei220324 <0vx05845724272k@ezweb.ne.jp>
2026-01-15 05:55:45 +0900

    API 取得

--- 16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ 16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Example = () => {
-  return <></>;
+  useEffect(() => {
+    const res = async () => {
+      const res = await axios.get("http://127.0.0.1:3003/user");
+      console.log(res.data);
+    };
+
+    res();
+  }, []);
+
+  return <div>Example</div>;
 };
 
 export default Example;
